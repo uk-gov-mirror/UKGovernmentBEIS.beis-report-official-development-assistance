aac4594742e138fe6ac2ec435b7ee2f1defe9f86
bob walker <bob@dxw.com>
2020-03-11 16:40:14 +0000

    WIP

--- install-terraform-provider-for-cf.sh
+++ install-terraform-provider-for-cf.sh
@@ -9,10 +9,9 @@ then
   echo "installing Cloudfoundry Terraform plugin"
 
   mkdir -p ~/.terraform.d/plugins/linux_amd64
-  chmod +x ~/.terraform.d/plugins/linux_amd64
   curl -L https://github.com/cloudfoundry-community/terraform-provider-cf/releases/download/v0.11.0/terraform-provider-cloudfoundry_linux_amd64 \
     -o ~/.terraform.d/plugins/linux_amd64/terraform-provider-cloudfoundry
-  chmod +x  ~/.terraform.d/plugins/linux_amd64/terraform-provider-cloudfoundry
+  chmod u+x  ~/.terraform.d/plugins/linux_amd64/terraform-provider-cloudfoundry
 
 
   echo "finished downloading Cloudfoundry Terraform plugin"
