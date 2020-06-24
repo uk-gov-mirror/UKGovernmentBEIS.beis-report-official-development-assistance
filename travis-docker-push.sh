fbc6bcb5874b996f28594cf895fb9a574e60e01b
Tom Hipkin <tomhipkin@gmail.com>
2020-06-24 12:23:14 +0100

    Use caching when creating new production safe images
    We need to create a test version of the image so that test gems and dependencies are available. Whilst we would like to have full parity between the artefact we test against the one we ship there is a requirement for them to differ.
    
    Given this separation a first step to building the same image twice, from scratch, is to use a local cache from the test image. This should allow the production image to reuse docker images such as those in the base stage like Ruby. It may also be able to go someway into the dependencies stage, by reusing the install of Node.
    
    Before this change the build took 8 minutes 19 seconds. After this changes..
    
    This is how Travis say to install the new version of Docker which has buildkit available https://docs.travis-ci.com/user/docker/#installing-a-newer-docker-version. Without this block, the build errors with: x

--- travis-docker-push.sh
+++ travis-docker-push.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 TAG="${TRAVIS_BUILD_NUMBER}-${TRAVIS_COMMIT}"
+DOCKER_BUILDKIT="1"
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build --target web -t "thedxw/beis-report-official-development-assistance:$TAG" .
-docker push "thedxw/beis-report-official-development-assistance:$TAG"
+
+# Build a new image and use the "latest" image as a cache
+docker build . \
+  --target web \
+  --build-arg BUILDKIT_INLINE_CACHE=1 \
+  --cache-from "thedxw/beis-report-official-development-assistance:latest" \
+  --progress=plain \
+  -t "thedxw/beis-report-official-development-assistance:${TAG}"
+
+# Update this image to be the new latest for future cache use
+docker tag "thedxw/beis-report-official-development-assistance:${TAG}" \
+  "thedxw/beis-report-official-development-assistance:latest"
+
+docker push "${DOCKER_IMAGE}"
