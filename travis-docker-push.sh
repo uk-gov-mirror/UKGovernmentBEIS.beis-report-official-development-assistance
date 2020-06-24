10012b3259f66f844b54e40a3a693f78b45cdb33
Tom Hipkin <tomhipkin@gmail.com>
2020-06-24 18:57:23 +0100

    try removing inline build kit

--- travis-docker-push.sh
+++ travis-docker-push.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 TAG="${TRAVIS_BUILD_NUMBER}-${TRAVIS_COMMIT}"
-DOCKER_BUILDKIT="1"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+echo "*****************"
+docker images
 
 # Build a new image and use the "latest" image as a cache
 docker build . \
   --target web \
-  --build-arg BUILDKIT_INLINE_CACHE=1 \
-  --cache-from "thedxw/beis-report-official-development-assistance:latest" \
-  --progress=plain \
+  --cache-from "thedxw/beis-report-official-development-assistance:${TAG}-test" \
   -t "thedxw/beis-report-official-development-assistance:${TAG}"
-
-# Update this image to be the new latest for future cache use
-docker tag "thedxw/beis-report-official-development-assistance:${TAG}" \
-  "thedxw/beis-report-official-development-assistance:latest"
 
 docker push "${DOCKER_IMAGE}"
