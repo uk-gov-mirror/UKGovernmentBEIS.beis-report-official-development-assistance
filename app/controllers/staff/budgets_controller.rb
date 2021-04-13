1d5df2e4baf5a2d26504c99fd121728268f88e8a
Cristina <cristina@dxw.com>
2021-04-13 11:42:58 +0100

    WIP Permit providing org params

--- app/controllers/staff/budgets_controller.rb
+++ app/controllers/staff/budgets_controller.rb
@@ -65,7 +65,10 @@ class Staff::BudgetsController < Staff::BaseController
       :value,
       :financial_year,
       :currency,
-      :funding_type
+      :funding_type,
+      :providing_organisation_name,
+      :providing_organisation_type,
+      :providing_organisation_reference
     )
   end
 
